cc3a37d6298638713a1dc8fa189f941efc9d53d4
ccamel <camel.christophe@gmail.com>
2022-07-28 17:13:45 +0200

    chore(project): rename sidebar name

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  mySidebar: [
+  whitepaperSidebar: [
     {
       type: 'category',
       label: 'Whitepaper',
