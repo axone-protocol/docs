4e99c6f1d4ab06037b2e75bd5aee2534d54c2afb
ccamel <camel.christophe@gmail.com>
2023-07-30 19:09:45 +0200

    feat(whitepaper): add plot component & data series for token model

---
+++ docs/whitepaper/token-model.tsx
@@ -0,0 +1,119 @@
+import { ResponsiveLine } from '@nivo/line'
+import React from 'react'
+import { useColorMode } from '@docusaurus/theme-common'
+
+const nIntFormat = new Intl.NumberFormat('en')
+const nFloatFormat = new Intl.NumberFormat('en', {
+  minimumFractionDigits: 1,
+  maximumFractionDigits: 1
+})
+
+const a = 200000000
+const i = 0.0002
+const c = 2.5
+const bt = 0.66
+
+const srtdPoints = (): { x: number; y: number }[] => {
+  const data: { x: number; y: number }[] = []
+
+  for (let x = 0; x <= 1; x += 0.1) {
+    const y = a * i * (c - x / bt)
+    data.push({ x, y })
+  }
+
+  return data
+}
+
+export const srtdData = [
+  {
+    id: 'r',
+    color: 'hsl(331, 70%, 50%)',
+    data: srtdPoints()
+  }
+]
+
+const srdyPercentPoints = (): { x: number; y: number }[] => {
+  const data: { x: number; y: number }[] = []
+
+  for (let x = 0; x <= 1; x += 0.1) {
+    const y = 365 * i * (c - x / bt)
+
+    data.push({ x, y })
+  }
+
+  return data
+}
+
+export const srdyPercentData = [
+  {
+    id: 'percentage of a',
+    color: 'hsl(331, 70%, 50%)',
+    data: srdyPercentPoints()
+  }
+]
+
+export const LinePlot = ({ caption, xLegend, yLegend, xFormat, yFormat, data }) => {
+  const isDark = useColorMode().colorMode === 'dark'
+
+  return (
+    <figure>
+      <center>
+        <div className="plot" style={{ height: '380px', width: '570px', maxWidth: '100%' }}>
+          <ResponsiveLine
+            data={data}
+            margin={{ top: 20, right: 20, bottom: 50, left: 80 }}
+            lineWidth={3}
+            xScale={{ type: 'point' }}
+            yScale={{
+              type: 'linear',
+              min: 'auto',
+              max: 'auto'
+            }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: xLegend,
+              legendOffset: 36,
+              legendPosition: 'middle',
+              format: xFormat
+            }}
+            axisLeft={{
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: yLegend,
+              legendOffset: -60,
+              legendPosition: 'end',
+              format: yFormat
+            }}
+            yFormat={yFormat}
+            xFormat={xFormat}
+            enableGridX={false}
+            enableGridY={false}
+            pointSize={0}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabelYOffset={-12}
+            useMesh={true}
+            theme={{
+              textColor: isDark ? '#ffffff' : '#000000',
+              tooltip: {
+                container: {
+                  background: isDark ? '#000000' : '#ffffff'
+                }
+              }
+            }}
+          />
+        </div>
+      </center>
+      <center>
+        <figcaption>
+          <span className="math math-inline">{caption}</span>
+        </figcaption>
+      </center>
+    </figure>
+  )
+}
