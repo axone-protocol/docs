6cea9d038d336552017abafcfaf2c44350424387
ccamel <camel.christophe@gmail.com>
2023-07-31 09:33:21 +0200

    style(whitepaper): use theme color where possible

--- docs/whitepaper/token-model.tsx
+++ docs/whitepaper/token-model.tsx
@@ -1,44 +1,46 @@
 import { ResponsiveLine } from '@nivo/line'
 import React from 'react'
-import { useColorMode } from '@docusaurus/theme-common'
+import { usePrismTheme } from '@docusaurus/theme-common'
 
 const a = 200000000
 const i = 0.0002
 const c = 2.5
 const bt = 0.66
 
+const lineColor = 'hsl(331, 70%, 50%)' // TODO: use theme color
+
 function* srtdPoints(): Generator<{ x: number; y: number }> {
   for (let x = 0; x <= 1; x += 0.05) {
-    const y = a * i * (c - x / bt);
-    yield { x, y };
+    const y = a * i * (c - x / bt)
+    yield { x, y }
   }
 }
 
 export const srtdData = [
   {
     id: 'r',
-    color: 'hsl(331, 70%, 50%)',
+    color: lineColor,
     data: [...srtdPoints()]
   }
 ]
 
 function* srdyPercentPoints(): Generator<{ x: number; y: number }> {
   for (let x = 0; x <= 1; x += 0.05) {
-    const y = 365 * i * (c - x / bt);
-    yield { x, y };
+    const y = 365 * i * (c - x / bt)
+    yield { x, y }
   }
 }
 
 export const srdyPercentData = [
   {
     id: 'percentage of a',
-    color: 'hsl(331, 70%, 50%)',
+    color: lineColor,
     data: [...srdyPercentPoints()]
   }
 ]
 
 export const LinePlot = ({ caption, xLegend, yLegend, xFormat, yFormat, data }) => {
-  const isDark = useColorMode().colorMode === 'dark'
+  const prismTheme = usePrismTheme()
 
   return (
     <figure>
@@ -48,11 +50,11 @@ export const LinePlot = ({ caption, xLegend, yLegend, xFormat, yFormat, data }) 
             data={data}
             margin={{ top: 20, right: 20, bottom: 50, left: 80 }}
             lineWidth={3}
-            curve='linear'
-            xScale={{ 
+            curve="linear"
+            xScale={{
               type: 'linear',
               min: 'auto',
-              max: 'auto',
+              max: 'auto'
             }}
             yScale={{
               type: 'linear',
@@ -91,7 +93,7 @@ export const LinePlot = ({ caption, xLegend, yLegend, xFormat, yFormat, data }) 
                   strokeWidth: 1
                 },
                 textStyle: {
-                  fill: '#b0413e'
+                  fill: '#b0413e' // TODO: use theme color
                 },
                 value: bt
               }
@@ -104,10 +106,10 @@ export const LinePlot = ({ caption, xLegend, yLegend, xFormat, yFormat, data }) 
             pointLabelYOffset={-12}
             useMesh={true}
             theme={{
-              textColor: isDark ? '#ffffff' : '#000000',
+              textColor: prismTheme.plain.color,
               tooltip: {
                 container: {
-                  background: isDark ? '#000000' : '#ffffff'
+                  background: prismTheme.plain.backgroundColor
                 }
               }
             }}
